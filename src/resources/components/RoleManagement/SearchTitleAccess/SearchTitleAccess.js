e16116ade05bdd4871cabf172130b326cef87636
Keyvan <keyvan@Keyvans-MacBook-Pro.local>
2024-02-10 14:38:13 +0330

    repair searchBox for nested children and connected to v-model to get value of checkbox as object

--- src/resources/components/RoleManagement/SearchTitleAccess/SearchTitleAccess.js
+++ src/resources/components/RoleManagement/SearchTitleAccess/SearchTitleAccess.js
@@ -1,5 +1,35 @@
+import { useAccessManagementStore } from "@/stores/accessManagementStore";
+import { ref, watch } from "vue";
+
 export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
   setup() {
-    return {};
+    let selected = ref([]);
+    const store = useAccessManagementStore();
+    const {
+      set_select_categories_action,
+      get_actions_action,
+      mix_items_action,
+    } = store;
+
+    watch(selected, async (newSelected) => {
+      console.log(`new selected are ${JSON.stringify(newSelected)}`);
+      set_select_categories_action(JSON.stringify(newSelected));
+      const data = await get_actions_action(JSON.stringify(newSelected));
+      console.log("return of actions method: ", data);
+      console.log("newSelected.length: ", newSelected.length);
+      if (newSelected.length != 0) {
+        const convertArray = mix_items_action(data);
+        console.log("convertArray: ", convertArray);
+      }
+    });
+    return {
+      selected,
+    };
   },
 };
--- src/resources/components/RoleManagement/SearchTitleAccess/SearchTitleAccess.vue
+++ src/resources/components/RoleManagement/SearchTitleAccess/SearchTitleAccess.vue
@@ -2,38 +2,149 @@
   <div class="parent-categories-box d-flex flex-column">
     <div class="parent-search-text-input mb-3">
       <v-text-field
-        label="جستجوی عنوان دسترسی"
         prepend-inner-icon="mdi-magnify"
         variant="underlined"
         class="search-input-style"
         color="#ced1d4"
         base-color="#ced1d4"
-      ></v-text-field>
+      >
+        <template v-slot:label>
+          <span style="font-size: 14px"> جستجوی عنوان دسترسی</span>
+        </template>
+      </v-text-field>
     </div>
 
     <div class="parent-categories-checkbox">
       <div class="parent-each-checkbox-row">
-        <v-expansion-panels>
-          <v-expansion-panel
-            elevation="0"
-            bg-color="transparent"
-            selected-class="active-item"
-            class="custom-row"
+        <v-expansion-panels
+          v-for="(firstCategoryChild, i) in categories"
+          :key="i"
+        >
+          <div
+            class="d-flex flex-grow-1 flex-column"
+            v-if="firstCategoryChild.children.length == 0"
           >
-            <v-expansion-panel-title>
+            <v-checkbox
+              :label="firstCategoryChild.name"
+              :value="{
+                name: firstCategoryChild.name,
+                value: firstCategoryChild.id,
+              }"
+              v-model="selected"
+              color="#3e2356"
+              hide-details
+            ></v-checkbox>
+            <span
+              style="height: 1px; background-color: #c9c9c9; width: 100%"
+            ></span>
+          </div>
+
+          <v-expansion-panel v-else elevation="0" bg-color="transparent">
+            <v-expansion-panel-title class="d-flex flex-grow-1 custom-title">
               <v-checkbox
-                label="دسته بندی"
+                :label="firstCategoryChild.name"
+                :value="{
+                  name: firstCategoryChild.name,
+                  value: firstCategoryChild.id,
+                }"
+                v-model="selected"
                 color="#3e2356"
                 hide-details
               ></v-checkbox>
             </v-expansion-panel-title>
-            <hr style="margin-top: 0" />
-            <v-expansion-panel-text>
-              <v-checkbox
-                label="دسته بندی 1-1"
-                color="#3e2356"
-                hide-details
-              ></v-checkbox>
+            <span
+              style="
+                display: block;
+                height: 1px;
+                background-color: #c9c9c9;
+                width: 100%;
+              "
+            ></span>
+            <v-expansion-panel-text class="custom-style" :eager="true">
+              <v-expansion-panels
+                v-for="(secondCategoryChild, j) in firstCategoryChild.children"
+                :key="j + '_' + i"
+              >
+                <div
+                  class="d-flex flex-grow-1 flex-column"
+                  v-if="secondCategoryChild.children.length == 0"
+                >
+                  <v-checkbox
+                    :label="secondCategoryChild.name"
+                    :value="{
+                      name: secondCategoryChild.name,
+                      value: secondCategoryChild.id,
+                    }"
+                    v-model="selected"
+                    color="#3e2356"
+                    hide-details
+                  ></v-checkbox>
+                  <span
+                    style="
+                      display: block;
+                      height: 1px;
+                      background-color: #c9c9c9;
+                      width: 100%;
+                    "
+                  ></span>
+                </div>
+
+                <v-expansion-panel v-else elevation="0" bg-color="transparent">
+                  <v-expansion-panel-title
+                    class="d-flex flex-grow-1 custom-title"
+                  >
+                    <v-checkbox
+                      :label="secondCategoryChild.name"
+                      :value="{
+                        name: secondCategoryChild.name,
+                        value: secondCategoryChild.id,
+                      }"
+                      v-model="selected"
+                      color="#3e2356"
+                      hide-details
+                    ></v-checkbox>
+                  </v-expansion-panel-title>
+                  <span
+                    style="
+                      display: block;
+                      height: 1px;
+                      background-color: #c9c9c9;
+                      width: 100%;
+                    "
+                  ></span>
+                  <v-expansion-panel-text>
+                    <!--start third-->
+                    <v-expansion-panels
+                      v-for="(
+                        thirdCategoryChild, k
+                      ) in secondCategoryChild.children"
+                      :key="k + '_' + j + '_' + i"
+                    >
+                      <div class="d-flex flex-grow-1 flex-column">
+                        <v-checkbox
+                          :label="thirdCategoryChild.name"
+                          :value="{
+                            name: thirdCategoryChild.name,
+                            value: thirdCategoryChild.id,
+                          }"
+                          v-model="selected"
+                          color="#3e2356"
+                          hide-details
+                        ></v-checkbox>
+                        <span
+                          style="
+                            display: block;
+                            height: 1px;
+                            background-color: #c9c9c9;
+                            width: 100%;
+                          "
+                        ></span>
+                      </div>
+                    </v-expansion-panels>
+                    <!--third-->
+                  </v-expansion-panel-text>
+                </v-expansion-panel>
+              </v-expansion-panels>
             </v-expansion-panel-text>
           </v-expansion-panel>
         </v-expansion-panels>
